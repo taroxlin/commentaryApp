34c6b459c63fc19cb5d649d0239593804a72e0b9
Taroxlin <bul41991@gmail.com>
2020-02-23 12:20:46 +0100

    Updates

---
+++ precache-manifest.55561eb08c77f0735ec09fd20bdefb86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145b8651c363e1e3e1a24d4aad647c24",
+    "url": "/commentaryApp/index.html"
+  },
+  {
+    "revision": "f4fae329b2f3c748724c",
+    "url": "/commentaryApp/static/css/main.5dcebec1.chunk.css"
+  },
+  {
+    "revision": "03777ad619f43bbc9e99",
+    "url": "/commentaryApp/static/js/2.26da76e2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/commentaryApp/static/js/2.26da76e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4fae329b2f3c748724c",
+    "url": "/commentaryApp/static/js/main.d710f414.chunk.js"
+  },
+  {
+    "revision": "a821872cffa6f35aa2f5",
+    "url": "/commentaryApp/static/js/runtime-main.67503b6f.js"
+  }
+]);
